d68bd0b2d5edf496f0bda150148a59c437f5a649
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-03-18 10:59:50 -0300

    aula 07 - Lazy Loading
    Vamos rodar este código deixando a última linha syso comentada. A conta é buscada por causa da query, e os titulares foram trazidos. Porém, no lugar das movimentações, nada aparece, obviamente por termos comentado a linha relativa a isso. Veremos o que ocorre quando descomentamos esta linha e rodamos a aplicação novamente.
    
    Feito o select (a busca pela conta), várias buscas pelas movimentações de cada uma das contas foram disparadas. Enquanto a conta da Maria dos Santos é tratada, por exemplo, faz-se outro select para buscar suas movimentações. No caso, não existe nenhuma, então nada foi preenchido. Isso se repete com os outros titulares e todas as contas no foreach.
    
    Reparem, no entanto, que se fez isso exatamente quando chamamos, pois quando tínhamos a linha comentada, estas queries não haviam sido feitas. Esta consequência se chama N + 1, por realizar 1 select para buscar as contas e, para cada uma delas, no laço, foi feito mais um select para buscar as movimentações correspondentes.
    
    Isso ocorre justamente porque, por padrão, os relacionamentos [...]ToMany, que são listas ou conjuntos, são considerados Lazy, com "carregamento preguiçoso". Ou seja, o JPA só irá ao banco para buscar estes relacionamentos quando você precisa deles, ao chamarmos o método getMovimentacoes(), por exemplo.
    
    Porém, podemos não querer isto e, pela quantidade de queries feitas, queremos que quando a conta for buscada, sejam trazidas também as movimentações. Então, precisaremos substituir o comportamento Lazy por Eager Loading, a partir do parâmetro join fetch, o que quer dizer que queremos juntar, nesta query, a conta e a movimentação.
    
    Vamos rodar para verificar se tanto a conta quanto a movimentação são trazidas na mesma query? Veremos que o select é feito, bem como um inner join para a movimentação. Desta forma, conseguimos imprimir as movimentações que existem no banco.

---
+++ src/br/com/caelum/financas/teste/TesteMovimentacoesDasContas.java
@@ -0,0 +1,33 @@
+package br.com.caelum.financas.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteMovimentacoesDasContas {
+
+	public static void main(String[] args) {
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+//		join fetch realiza apenas um select trazendo as movimentacoes junto com a conta
+		String jpql = "select c from Conta c join fetch c.movimentacoes";
+		
+		Query query = em.createQuery(jpql);
+		
+		List<Conta> todasAsContas = query.getResultList();
+		
+		for(Conta conta : todasAsContas) {
+			System.out.println("Titular: "+conta.getTitular());
+			System.out.println("Movimentacoes: ");
+			System.out.println(conta.getMovimentacoes());
+		}
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}

dc320ba65e89c45e1298adf4a13c383473425a0b
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-03-18 11:57:24 -0300

    aula 08 - TypedQuery
    Retornar a media por dia e adicionar o typedQuery para tornar nosso codigo mais seguro.

--- src/br/com/caelum/financas/teste/TestesFuncoesJPQL.java
+++ src/br/com/caelum/financas/teste/TestesFuncoesJPQL.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.Movimentacao;
@@ -28,17 +29,19 @@ public class TestesFuncoesJPQL {
 		//media
 		String jpql = "select avg(m.valor) from Movimentacao m where m.conta = :pConta "
 				+ " and m.tipoMovimentacao = :pTipoMovimentacao " 
-				+ " order by m.valor desc"; // com named parameter
+				+ " group by day(m.data), month(m.data), year(m.data)"; // com named parameter
 		
-		Query query = em.createQuery(jpql); // adiciona a jpql na query
+		TypedQuery<Double> query = em.createQuery(jpql, Double.class); // adiciona a jpql na query
 		query.setParameter("pConta", conta); // adiciona o parametro de busca
 		query.setParameter("pTipoMovimentacao", TipoMovimentacao.SAIDA); // adiciona o parametro de busca
 
 //		BigDecimal soma = (BigDecimal) query.getSingleResult();
-		Double media = (Double) query.getSingleResult();
+		List<Double> medias = (List<Double>) query.getResultList();
 
 //		System.out.println("A soma é : "+ soma);
-		System.out.println("A média é : "+ media);
+		System.out.println("A média é : "+ medias.get(0));
+		System.out.println("A média é : "+ medias.get(1));
+		System.out.println("A média é : "+ medias.get(2));
 		em.getTransaction().commit();
 		em.close();
 	}
